76f9c103ff4ee9f4f2ccc17eca7675e774247eec
Cipta <42090335+winterbloomingrose@users.noreply.github.com>
2021-04-17 12:49:35 -0400

    Completed Stretch Tasks
    Pretty proud of my car function even if it isnt fully optimized and can be improved

--- index.js
+++ index.js
@@ -94,19 +94,29 @@ function Airplane(name) {
     this.tank = 0;
     this.odometer = 0;
     Car.prototype.fill = function (gallons) {
-      if (this.gallons >= 0) {
         this.tank = this.tank + gallons;
         return this.tank;
       }
+    Car.prototype.drive = function (distance) {
+      const maxDistance = this.tank * milesPerGallon;
+      this.tank = this.tank - (distance / milesPerGallon);
+      if (this.tank <= 0) {
+        this.odometer = this.odometer + maxDistance;
+        return `I ran out of fuel at ${this.odometer} miles on a ${distance} mile trip!`
+      }
       else {
-        return this.gallons;
+        this.odometer = this.odometer + distance;
+        return `My odometer reads ${this.odometer} miles, and there is ${this.tank} gallons of gas left in me!`
       }
     }
   }
-  
-  const lightningMcQueen = new Car("sapient", 50)
-  console.log(lightningMcQueen.fill("50"))
-  
+  /* i think there should be a way to do the run out of fuel w a for loop but eh */ 
+
+  const lightningMcQueen = new Car("sapient", 20)
+  console.log(lightningMcQueen.fill(20)) /* it can drive 400 miles */
+  console.log(lightningMcQueen.drive(200)) /* using some gas*/
+  console.log(lightningMcQueen.drive(500)) /* using all gas*/
+
   /*
     TASK 3
       - Write a Baby constructor subclassing Person.
@@ -117,21 +127,25 @@ function Airplane(name) {
  function Baby(name, age, favoriteToy) {
     Person.call(this, name, age);
     this.favoriteToy = favoriteToy;
+    Baby.prototype = Object.create(Person.prototype)
     Baby.prototype.play = function(favoriteToy) {
       return `Playing with ${this.favoriteToy}`;
     }
   }
  
-  
+  const spawnling =  new Baby("spawn", 3, "barbed wire");
+  console.log(spawnling);
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. window binding: the "this" keyword is forced to be the child of the window/global scope. there is nothing "closer" for it to bind to and it is outside any particular object.
-    2. implicit binding: it is inside of a specific declared object. it binds to this object. 
-    3. 'new' binding: it binds to a 'new'ly constructed constant variable  that is created with a constructor function. 'new' directs 'this' to the newly created object.
-    4. explicit binding: using 'call' 'bind' or 'apply' to point 'this' to a certain value.
+      'this' references the object executing the current function
+      
+      1. window binding: the "this" keyword is forced to be the child of the window/global scope. there is nothing "closer" for it to bind to and it is outside any particular object.
+      2. implicit binding: it is inside of a specific declared object. it binds to this object. 
+      3. 'new' binding: it binds to a 'new'ly constructed constant variable  that is created with a constructor function. 'new' directs 'this' to the newly created object.
+      4. explicit binding: using 'call' 'bind' or 'apply' to point 'this' to a certain value.
 
-    references for myself: https://alligator.io/js/this-keyword/ 
+      references for myself: https://alligator.io/js/this-keyword/ , https://www.youtube.com/watch?v=gvicrj31JOM 
   */
   
   
